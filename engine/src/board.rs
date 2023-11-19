d19f49d5a7782bea688456cc8283e87bb01a0f26
Nicholas de Jong <nickacidegamer@gmail.com>
2023-11-19 23:58:53 +0200

    movegen rewrite

--- engine/src/board.rs
+++ engine/src/board.rs
@@ -1,9 +1,10 @@
 use types::bitboard::BitBoard;
 use types::color::Color;
-use types::nonsliders::common::{KNIGHT_MOVES, KING_MOVES};
+use types::nonsliders::common::{KNIGHT_MOVES, KING_MOVES, PAWN_ATTACKS};
 use types::piece::Piece;
-use types::r#move::Move;
-use types::sliders::dumb7fill::{bishop_moves, rook_moves};
+use types::moves::PieceMoves;
+use types::slider::Slider;
+// use types::sliders::dumb7fill::{bishop_moves, rook_moves};
 use types::sliders::magic::magic_index;
 use types::square::Square;
 use types::sliders::common::{TABLE_SIZE, ROOK_BLOCKERS, BISHOP_BLOCKERS, ROOK_MAGICS, ROOK_SHIFT, BISHOP_MAGICS, BISHOP_SHIFT, BISHOP_ATTACKS, ROOK_ATTACKS, ROOK_OFFSETS, BISHOP_OFFSETS};
@@ -20,9 +21,16 @@ pub struct Board {
     all: [BitBoard; 2], // white, black
     turn: Color,
     castling: [(bool, bool); 2], // [(K, Q), (k, q)]
-    enpassant: Option<Square>,
+    enpassant: BitBoard,
     halfmoves: u8,
     fullmoves: u16,
+}
+
+struct Masks {
+    checkmask: BitBoard,
+    diagonal: BitBoard,
+    orthagonal: BitBoard,
+    en_pessant: BitBoard // Contains mask of "pinned" enemy pawns preventing illegal en pessant
 }
 
 fn piece_idx(piece: char) -> usize {
@@ -106,7 +114,7 @@ impl std::str::FromStr for Board {
                     return Err(FENParseError);
                 }
                 if (board.turn == Color::White && 1 << (sq - 8) & board.pieces[1][5].0 > 0) || (board.turn == Color::Black && 1 << (sq + 8) & board.pieces[0][5].0 > 0) {
-                    board.enpassant = Some(Square(sq));
+                    board.enpassant = Square(sq).bitboard();
                 }
             } else {
                 return Err(FENParseError);
@@ -132,7 +140,7 @@ impl Board {
         Board {
             all: [BitBoard::EMPTY; 2],
             castling: [(false, false); 2],
-            enpassant: None,
+            enpassant: BitBoard::EMPTY,
             halfmoves: 0,
             fullmoves: 0,
             pieces: [[BitBoard::EMPTY; 6]; 2],
@@ -154,7 +162,7 @@ impl Board {
 
 
     /// Calculates checkmask and pinmask for current position and color
-    fn checkmask_pinmask(&self) -> (BitBoard /* Checkmask */, BitBoard /* Orthagonal */, BitBoard /* Diagonal */) {
+    fn masks(&self) -> Masks {
         let color = self.turn as usize;
         let king = self.pieces[color][0].0.trailing_zeros() as usize;
         let enemy = self.pieces[1 - color];
@@ -164,6 +172,7 @@ impl Board {
 
         let mut checkmask = BitBoard::EMPTY;
         let mut pinmask = (BitBoard::EMPTY, BitBoard::EMPTY);
+        let mut en_pessant = BitBoard::EMPTY;
 
         for attacker in orthagonal {
             let between = squares_between(Square(king), attacker);
@@ -183,18 +192,21 @@ impl Board {
             if (between & self.us()).len() == 1 && (between & self.them()).len() == 0 {
                 pinmask.1 |= between | attacker.bitboard();
             }
+            if (between & enemy[5]).len() == 1 {
+                en_pessant |= between & enemy[5];
+            }
         }
 
         if checkmask.is_empty() {
             checkmask = BitBoard::FULL;
         }
-        if pinmask.0.is_empty() {
-            pinmask.0 = BitBoard::FULL;
+
+        Masks {
+            checkmask,
+            orthagonal: pinmask.0,
+            diagonal: pinmask.1,
+            en_pessant
         }
-        if pinmask.1.is_empty() {
-            pinmask.1 = BitBoard::FULL;
-        }
-        (checkmask, pinmask.0, pinmask.1)
     }
 
     /// Returns mask containing all squares attacked by enemy pieces
@@ -207,9 +219,17 @@ impl Board {
         // king
         mask |= BitBoard(KING_MOVES[enemy[0].0.trailing_zeros() as usize]);
         // bishops / queen diagonals
-        mask |= BitBoard(bishop_moves(enemy[1].0 | enemy[3].0, self.occupied().0));
+        for piece in enemy[1] | enemy[3] {
+            let blockers = BitBoard(BISHOP_BLOCKERS[piece.0]) & self.occupied();
+            let idx = magic_index(blockers, BISHOP_MAGICS[piece.0], BISHOP_SHIFT) + BISHOP_OFFSETS[piece.0];
+            mask |= BitBoard(SLIDER_TABLE[idx]);
+        }
         // rooks / queen orthagonals
-        mask |= BitBoard(rook_moves(enemy[1].0 | enemy[2].0, self.occupied().0));
+        for piece in enemy[1] | enemy[2] {
+            let blockers = BitBoard(ROOK_BLOCKERS[piece.0]) & self.occupied();
+            let idx = magic_index(blockers, ROOK_MAGICS[piece.0], ROOK_SHIFT) + ROOK_OFFSETS[piece.0];
+            mask |= BitBoard(SLIDER_TABLE[idx]);
+        }
         // knights
         for knight in enemy[4] {
             mask |= BitBoard(KNIGHT_MOVES[knight.0]);
@@ -219,8 +239,8 @@ impl Board {
 
     // FIXME: Untested, especially pawn moves
     /// Returns all moves for current position and color
-    pub fn moves(&self) -> Vec<Move> {
-        let (checkmask, orthagonal, diagonal) = self.checkmask_pinmask();
+    pub fn moves(&self) -> Vec<PieceMoves> {
+        let Masks {checkmask, orthagonal, diagonal, en_pessant} = self.masks();
         let mut moves = vec![];
         let color = self.turn as usize;
         let pieces = self.pieces[color];
@@ -238,141 +258,127 @@ impl Board {
                 bb |= BitBoard(king.bitboard().0 >> 2);
             }
         }
-        for sq in bb & !self.us() & danger {
-            moves.push(Move {
-                piece: Piece::King,
-                from: king,
-                to: sq,
-                promotion: None
-            });
-        }
+        moves.push(PieceMoves {
+            piece: Piece::King,
+            from: king,
+            moves: bb & !self.us() & danger
+        });
 
-        if (checkmask & self.them()).len() > 1 {
+        if (checkmask & self.them()).len() > 1 && checkmask != BitBoard::FULL {
             // Double check, only generate king moves
             return moves;
         }
 
         let knights = pieces[4] & !(orthagonal | diagonal);
         for from in knights {
-            let bb = BitBoard(KNIGHT_MOVES[from.0]) & checkmask & !self.us();
-            for sq in bb {
-                moves.push(Move {
-                    piece: Piece::Knight,
-                    from,
-                    to: sq,
-                    promotion: None
-                });
-            }
+            moves.push(PieceMoves {
+                piece: Piece::Knight,
+                from,
+                moves: BitBoard(KNIGHT_MOVES[from.0]) & checkmask & !self.us()
+            });
         }
 
-        let rooks = pieces[2] & !diagonal;
+        // Pinless rook + queen moves
+        let rooks = (pieces[1] | pieces[2]) & !(orthagonal | diagonal);
         for from in rooks {
             let blockers = BitBoard(ROOK_BLOCKERS[from.0]) & self.occupied();
             let idx = magic_index(blockers, ROOK_MAGICS[from.0], ROOK_SHIFT) + ROOK_OFFSETS[from.0];
-            let bb = BitBoard(SLIDER_TABLE[idx]) & checkmask & !self.us();
-            for sq in bb {
-                moves.push(Move {
-                    piece: Piece::Rook,
-                    from,
-                    to: sq,
-                    promotion: None
-                });
-            }
+            moves.push(PieceMoves {
+                piece: [Piece::Queen, Piece::Rook][(pieces[2] & from.bitboard()).signum()],
+                from,
+                moves: BitBoard(SLIDER_TABLE[idx]) & checkmask & !self.us()
+            });
         }
 
-        let bishops = pieces[3] & !orthagonal;
+        // Pinless bishop + queen moves
+        let bishops = (pieces[1] | pieces[3]) & !(orthagonal | diagonal);
         for from in bishops {
             let blockers = BitBoard(BISHOP_BLOCKERS[from.0]) & self.occupied();
             let idx = magic_index(blockers, BISHOP_MAGICS[from.0], BISHOP_SHIFT) + BISHOP_OFFSETS[from.0];
-            let bb = BitBoard(SLIDER_TABLE[idx]) & checkmask & !self.us();
-            for sq in bb {
-                moves.push(Move {
-                    piece: Piece::Bishop,
-                    from,
-                    to: sq,
-                    promotion: None
-                });
-            }
+            dbg!(idx);
+            dbg!(SLIDER_TABLE[idx], Slider::Bishop.pseudo_moves(from, blockers));
+
+            // FIXME: Fix hash collisions
+            // moves.push(PieceMoves {
+            //     piece: [Piece::Queen, Piece::Bishop][(pieces[3] & from.bitboard()).signum()],
+            //     from,
+            //     moves: BitBoard(SLIDER_TABLE[idx]) & checkmask & !self.us()
+            // });
         }
 
-        // Diagonal queen moves
-        let queens = pieces[1] & !orthagonal;
-        for from in queens {
+        // Diagonally pinned queens and bishops
+        let diagonal = (pieces[1] | pieces[3]) & diagonal;
+        for from in diagonal {
             let blockers = BitBoard(BISHOP_BLOCKERS[from.0]) & self.occupied();
             let idx = magic_index(blockers, BISHOP_MAGICS[from.0], BISHOP_SHIFT) + BISHOP_OFFSETS[from.0];
-            let bb = BitBoard(SLIDER_TABLE[idx]) & checkmask & !self.us();
-            for sq in bb {
-                moves.push(Move {
-                    piece: Piece::Queen,
-                    from,
-                    to: sq,
-                    promotion: None
-                });
-            }
+            moves.push(PieceMoves {
+                piece: [Piece::Queen, Piece::Bishop][(pieces[3] & from.bitboard()).signum()],
+                from,
+                moves: BitBoard(SLIDER_TABLE[idx]) & checkmask & !self.us() & diagonal
+            });
         }
-        // Orthagonal queen moves
-        let queens = pieces[1] & !diagonal;
-        for from in queens {
+
+        // Orthagonally pinned queens and rooks
+        let orthagonal = (pieces[1] | pieces[2]) & orthagonal;
+        for from in orthagonal {
             let blockers = BitBoard(ROOK_BLOCKERS[from.0]) & self.occupied();
             let idx = magic_index(blockers, ROOK_MAGICS[from.0], ROOK_SHIFT) + ROOK_OFFSETS[from.0];
-            let bb = BitBoard(SLIDER_TABLE[idx]) & checkmask & !self.us();
-            for sq in bb {
-                moves.push(Move {
-                    piece: Piece::Queen,
-                    from,
-                    to: sq,
-                    promotion: None
-                });
-            }
+            moves.push(PieceMoves {
+                piece: [Piece::Queen, Piece::Bishop][(pieces[2] & from.bitboard()).signum()],
+                from,
+                moves: BitBoard(SLIDER_TABLE[idx]) & checkmask & !self.us() & orthagonal
+            });
         }
 
-        // Pinned pawn pushes
-        let pawns = pieces[5] & orthagonal;
-        let mut bb = BitBoard(pawns.0 << 8) & !self.occupied() & checkmask & orthagonal;
-        // Pinned pawn captures
-        let pawns = pieces[5] & diagonal;
-        let captures = BitBoard(((pawns & BitBoard::NOT_A).0 << 7 | (pawns & BitBoard::NOT_H).0 << 9) >> (16 * color));
-        bb |= captures & self.all[1 - color] & checkmask & diagonal;
-        // Unpinned pawn pushes and captures
-        let pawns = pieces[5] & !(orthagonal | diagonal);
-        bb |= BitBoard(pawns.0 << 8) & !self.occupied() & checkmask;
-        let captures = BitBoard(((pawns & BitBoard::NOT_A).0 << 7 | (pawns & BitBoard::NOT_H).0 << 9) >> (16 * color));
-        bb |= captures & self.all[1 - color] & checkmask;
+        let push_white = |x: u64| x << 8;
+        let push_black = |x: u64| x >> 8;
+        let push_forward = [push_white, push_black][color];
+        let second_rank = BitBoard([0xff00, 0xff000000000000][color]);
 
-        // TODO: en passant
+        // Orthagonally pinned pawns
+        let pawns = pieces[5] & orthagonal;
+        for from in pawns {
+            let mut bb = BitBoard(push_forward(from.bitboard().0)) & !self.occupied() & orthagonal;
+            if !(second_rank & from.bitboard()).is_empty() && !bb.is_empty() {
+                bb |= BitBoard(push_forward(bb.0));
+            }
+            moves.push(PieceMoves {
+                piece: Piece::Pawn,
+                from,
+                moves: bb & checkmask
+            });
+        }
+
+        // Diagonally pinned pawns
+        let pawns = pieces[5] & diagonal;
+        for from in pawns {
+            moves.push(PieceMoves {
+                piece: Piece::Pawn,
+                from,
+                moves: BitBoard(PAWN_ATTACKS[color][from.0]) & checkmask & diagonal & self.all[1 - color]
+            });
+        }
+
+        // Pinless pawn pushes and captures
+        let pawns = pieces[5] & !(orthagonal | diagonal);
+        for from in pawns {
+            // captures
+            let mut bb = BitBoard(PAWN_ATTACKS[color][from.0]) & (self.all[1 - color] | (BitBoard(push_forward(self.enpassant.0)) & !en_pessant));
+            // single push
+            let single = BitBoard(push_forward(from.bitboard().0)) & !self.occupied();
+            bb |= single;
+            // double push
+            if !single.is_empty() {
+                bb |= BitBoard(push_forward(single.0)) & !self.occupied();
+            }
+
+            moves.push(PieceMoves {
+                piece: Piece::Pawn,
+                from,
+                moves: bb & checkmask
+            });
+        }
 
         moves
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::str::FromStr;
-
-    use super::*;
-    #[test]
-    fn test_board_pieces() {
-        let brd = Board::default();
-        assert_eq!(brd.pieces[0], [BitBoard(16), BitBoard(8), BitBoard(129), BitBoard(36), BitBoard(66), BitBoard(65280)]);
-        assert_eq!(brd.pieces[1], [BitBoard(0x1000000000000000), BitBoard(0x800000000000000), BitBoard(0x8100000000000000), BitBoard(0x2400000000000000), BitBoard(0x4200000000000000), BitBoard(0xff000000000000)]);
-
-        let brd = Board::from_str("3nbrqk/3ppppp/8/8/8/8/PPPPP3/KQRBN3 w - - 0 1").unwrap();
-        assert_eq!(brd.pieces[0], [BitBoard(1), BitBoard(2), BitBoard(4), BitBoard(8), BitBoard(16), BitBoard(0x1f00)]);
-        assert_eq!(brd.pieces[1], [BitBoard(0x8000000000000000), BitBoard(0x4000000000000000), BitBoard(0x2000000000000000), BitBoard(0x1000000000000000), BitBoard(0x800000000000000), BitBoard(0xf8000000000000)]);
-    }
-
-    #[test]
-    fn test_checkmask_pinmask() {
-        let brd = Board::default();
-        let masks = brd.checkmask_pinmask();
-        assert_eq!(masks, (BitBoard::FULL, BitBoard::FULL, BitBoard::FULL));
-
-        let brd = Board::from_str("8/2k3b1/8/8/8/2K5/8/8 w - - 0 1").unwrap();
-        let masks = brd.checkmask_pinmask();
-        assert_eq!(masks, (BitBoard(0x40201008000000), BitBoard::FULL, BitBoard::FULL));
-
-        let brd = Board::from_str("8/8/5b2/8/3N4/2K5/8/8 w - - 0 1").unwrap();
-        let masks = brd.checkmask_pinmask();
-        assert_eq!(masks, (BitBoard::FULL, BitBoard::FULL, BitBoard(0x201008000000)));
     }
 }
